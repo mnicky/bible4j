91e20b4d885235dc3fc4931bfcd4d17b5c0a1e26
mnicky <xmnicky@gmail.com>
2011-05-04 22:28:24 +0200

    Utils class added

---
+++ src/main/java/com/github/mnicky/bible4j/Utils.java
@@ -0,0 +1,93 @@
+package com.github.mnicky.bible4j;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.mnicky.bible4j.data.BibleBook;
+
+public final class Utils {
+    
+    //FIXME: repair filepath
+    private static final String BIBLE_BOOK_ABBRS_FILE = "src/main/resources/bibleBookAbbrs.conf";
+    private static final String COMMENT_CHAR = "#";
+    private static final String SPLIT_CHAR = ":";
+    
+    /**
+     * This is static library class, therefore it is not possible to instantiate it.
+     */
+    private Utils() {}
+    
+    public static BibleBook getBibleBookNameByAbbr(String abbr) {
+	
+	Map<String, BibleBook> bookNames;
+	try {
+	    bookNames = getAbbrBookMap();
+	} catch (IOException e) {
+	    throw new RuntimeException("BibleBook name could not be retrieved.", e);
+	}
+	
+	return bookNames.get(abbr);
+	
+    }
+
+    public static Map<String, BibleBook> getAbbrBookMap() throws IOException {
+	Map<String, BibleBook> bookNames = new HashMap<String, BibleBook>();
+	BufferedReader r = null;
+	String line;
+
+	try {
+	    r = new BufferedReader(new InputStreamReader(new FileInputStream(BIBLE_BOOK_ABBRS_FILE), "utf-8"));
+
+	    while ((line = r.readLine()) != null) {
+		if (line.length() > 0 && !line.startsWith(COMMENT_CHAR)) {
+		    String[] abbrAndName = line.split(SPLIT_CHAR);
+		    if (abbrAndName.length > 2)
+			throw new RuntimeException("Bad format in configuration file " + BIBLE_BOOK_ABBRS_FILE);
+		    //System.out.println(abbrAndName[0] + " " + abbrAndName[1]);
+		    bookNames.put(abbrAndName[0], BibleBook.getBibleBookByName(abbrAndName[1]));
+		}
+	    }
+	} finally {
+	    r.close();
+	}
+
+	return bookNames;
+    }
+    
+    public static Map<BibleBook, String> getBookAbbrMap() throws IOException {
+	Map<BibleBook, String> bookNames = new HashMap<BibleBook, String>();
+	BufferedReader r = null;
+	String line;
+
+	try {
+	    r = new BufferedReader(new InputStreamReader(new FileInputStream(BIBLE_BOOK_ABBRS_FILE), "utf-8"));
+
+	    while ((line = r.readLine()) != null) {
+		if (line.length() > 0 && !line.startsWith(COMMENT_CHAR)) {
+		    String[] abbrAndName = line.split(SPLIT_CHAR);
+		    if (abbrAndName.length > 2)
+			throw new RuntimeException("Bad format in configuration file " + BIBLE_BOOK_ABBRS_FILE);
+		    bookNames.put(BibleBook.getBibleBookByName(abbrAndName[1]), abbrAndName[0]);
+		}
+	    }
+	} finally {
+	    r.close();
+	}
+
+	return bookNames;
+    }
+    
+    
+    
+    //for testing purposes
+    public static void main(String[] args) {
+	System.out.println(Utils.getBibleBookNameByAbbr("1pt"));
+	System.out.println("finished");
+    }
+
+}
