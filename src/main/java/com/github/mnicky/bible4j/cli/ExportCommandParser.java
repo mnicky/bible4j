a718835fa5ee84a663c7be3eaf34a429eb99e1fa
mnicky <xmnicky@gmail.com>
2011-05-05 22:58:17 +0200

    ExportCommandParser functionality completed

--- src/main/java/com/github/mnicky/bible4j/cli/ExportCommandParser.java
+++ src/main/java/com/github/mnicky/bible4j/cli/ExportCommandParser.java
@@ -1,22 +1,59 @@
 package com.github.mnicky.bible4j.cli;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import com.github.mnicky.bible4j.data.BibleVersion;
+import com.github.mnicky.bible4j.formats.BibleExporter;
+import com.github.mnicky.bible4j.formats.BibleExporterException;
+import com.github.mnicky.bible4j.formats.OsisBibleExporter;
 import com.github.mnicky.bible4j.storage.BibleStorage;
+import com.github.mnicky.bible4j.storage.BibleStorageException;
 
 public class ExportCommandParser extends CommandParser {
+    
+    OutputStream output;
+    private BibleVersion version;
 
     public ExportCommandParser(BibleStorage bibleStorage) {
 	super(bibleStorage);
     }
 
     @Override
-    public void parse(String[] args) {
-	// TODO Auto-generated method stub
+    public void parse(String[] args) throws BibleExporterException, BibleStorageException {
+	    try {
+		output = parseOutputStream(args);
+	    } catch (FileNotFoundException e) {
+		throw new BibleExporterException("File not found.", e);
+	    }
+	    version = parseVersionsAndReturnFirstIfEmpty(args).get(0);
+    }
+    
+    public void exportBible() throws BibleStorageException, BibleExporterException {
+	BibleExporter exporter = new OsisBibleExporter(bibleStorage);
+	exporter.exportBible(version, output);
+    }
 
+    private OutputStream parseOutputStream(String[] args) throws FileNotFoundException {
+	FileOutputStream out = new FileOutputStream(getFirstValue(args));
+	return out;
     }
 
     @Override
     public void printHelp() {
-	// TODO Auto-generated method stub
+	System.out.println("Usage:");
+        System.out.println("\t" + CommandParserLauncher.EXPORT_COMMAND + " PATH_TO_FILE [" + BIBLE_VERSION_ARGUMENT + " BIBLE_VERSION]");
+        
+        System.out.println();
+        System.out.println("\tPATH_TO_FILE \t path to OSIS file containing the Bible");
+        System.out.println("\tBIBLE_VERSION \t Bible version abbreviation (if no bible version is specified, the first Bible found is used)");
+        
+        System.out.println();
+        System.out.println("Examples:");
+        
+        System.out.println();
+        System.out.println("\t" + CommandParserLauncher.EXPORT_COMMAND + " kjv-osis-bible.xml " + BIBLE_VERSION_ARGUMENT + " kjv");
 	
     }
 
