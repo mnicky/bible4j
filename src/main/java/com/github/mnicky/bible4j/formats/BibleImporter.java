9c58f79cf80bf841d733ede1cfabea6b375a7f6c
mnicky <xmnicky@gmail.com>
2011-05-06 21:02:42 +0200

    small changes on api design

--- src/main/java/com/github/mnicky/bible4j/formats/BibleImporter.java
+++ src/main/java/com/github/mnicky/bible4j/formats/BibleImporter.java
@@ -2,9 +2,12 @@ package com.github.mnicky.bible4j.formats;
 
 import java.io.InputStream;
 
+import com.github.mnicky.bible4j.storage.BibleStorage;
 import com.github.mnicky.bible4j.storage.BibleStorageException;
 
 public interface BibleImporter {
+    
+    void setStorage(BibleStorage storage);
 
     void importBible(InputStream input) throws BibleImporterException, BibleStorageException;
 
