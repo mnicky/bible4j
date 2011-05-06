2d15982226c77c649a6082a1d953ded71b025a3a
mnicky <xmnicky@gmail.com>
2011-05-06 09:49:46 +0200

    dictionaryCommandParser functionality completed

--- src/main/java/com/github/mnicky/bible4j/cli/DictionaryCommandParser.java
+++ src/main/java/com/github/mnicky/bible4j/cli/DictionaryCommandParser.java
@@ -1,23 +1,58 @@
 package com.github.mnicky.bible4j.cli;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.github.mnicky.bible4j.data.DictTerm;
 import com.github.mnicky.bible4j.storage.BibleStorage;
+import com.github.mnicky.bible4j.storage.BibleStorageException;
+import com.github.mnicky.bible4j.storage.H2DbBibleStorage;
 
 public class DictionaryCommandParser extends CommandParser {
+    
+    private DictTerm dictTerm;
 
     public DictionaryCommandParser(BibleStorage bibleStorage) {
 	super(bibleStorage);
     }
 
     @Override
-    public void parse(String[] args) {
-	// TODO Auto-generated method stub
+    public void parse(String[] args) throws BibleStorageException {
+	dictTerm = parseDictTerm(getFirstValue(args));
+    }
+    
+    public DictTerm getDictTerm() {
+	return dictTerm;
+    }
 
+    private DictTerm parseDictTerm(String name) throws BibleStorageException {
+	return bibleStorage.getDictTerm(name);
     }
 
     @Override
     public void printHelp() {
-	// TODO Auto-generated method stub
+	System.out.println("Usage:");
+        System.out.println("\t" + CommandParserLauncher.DICTIONARY_COMMAND + " TERM_NAME");
+        
+        System.out.println();
+        System.out.println("\tTERM_NAME \t Name of the term to look up in the dictionary (case insensitive)");
+        
+        System.out.println();
+        System.out.println("Examples:");
+        
+        System.out.println();
+        System.out.println("\t" + CommandParserLauncher.DICTIONARY_COMMAND + " israel");
 	
+    }
+    
+    
+    public static void main(String[] args) throws SQLException, BibleStorageException {
+	BibleStorage storage = new H2DbBibleStorage(DriverManager.getConnection("jdbc:h2:tcp://localhost/test", "test", ""));
+	DictionaryCommandParser p = new DictionaryCommandParser(storage);
+	//storage.insertDictTerm(new DictTerm("Jehovah", "Hebrew 'name' for God, meaning 'I am'"));
+	String[] params = {"jehovah"};
+	p.parse(params);
+	System.out.println(p.getDictTerm());
     }
 
 }
