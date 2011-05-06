d2f8601f3035e83dbfe873e77e7d45b010f7df3f
mnicky <xmnicky@gmail.com>
2011-05-06 13:02:03 +0200

    unused imports removal

--- src/main/java/com/github/mnicky/bible4j/formats/BibleImporter.java
+++ src/main/java/com/github/mnicky/bible4j/formats/BibleImporter.java
@@ -2,10 +2,6 @@ package com.github.mnicky.bible4j.formats;
 
 import java.io.InputStream;
 
-import javax.xml.stream.FactoryConfigurationError;
-import javax.xml.stream.XMLStreamException;
-
-import com.github.mnicky.bible4j.storage.BibleStorage;
 import com.github.mnicky.bible4j.storage.BibleStorageException;
 
 public interface BibleImporter {
