2dfb520b737bd81e598289788ea42303ec570ed1
mnicky <xmnicky@gmail.com>
2012-02-12 00:12:37 +0100

    default db url changed to '~/.bible4j/bible4j'

--- src/main/java/com/github/mnicky/bible4j/storage/H2DbBibleStorageFactory.java
+++ src/main/java/com/github/mnicky/bible4j/storage/H2DbBibleStorageFactory.java
@@ -12,47 +12,47 @@ import com.github.mnicky.bible4j.AppRunner;
  * BibleFactory factory, providing the H2DbBibleStorage - a BibleStorage backed by the H2 database.
  */
 public final class H2DbBibleStorageFactory implements BibleStorageFactory {
-    
+
     private final static Logger logger = LoggerFactory.getLogger(AppRunner.AppLogger.class);
-    
-    private String url = "jdbc:h2:~/bible4j";
-    
+
+    private String url = "jdbc:h2:~/.bible4j/bible4j";
+
     private String user = "bible4j";
-    
+
     //TODO password as char array
     private String password = "";
-    
+
     private static final String URL_PROPERTY_NAME = "db.url";
     private static final String USER_PROPERTY_NAME = "db.user";
     private static final String PASSWORD_PROPERTY_NAME = "db.pwd";
-    
+
     /**
      * The url, username and password for h2 database can be set by setter methods of this factory.
      * This factory method checks for presence of system properties 'db.url',
-     * 'db.user' and 'db.pwd' and if exists, their value overrides values set by setters and 
+     * 'db.user' and 'db.pwd' and if exists, their value overrides values set by setters and
      * is used when creating the storage. If the values aren't set neither by setter methods
      * nor by the properties, the factory's default values are used.
-     * 
+     *
      * @throws BibleStorageException when Bible storage could not be created
      */
     @Override
     public BibleStorage createBibleStorage() {
 	try {
-	    
+
 	    if (System.getProperty(URL_PROPERTY_NAME) != null)
 		url = System.getProperty(URL_PROPERTY_NAME);
-	    
+
 	    if (System.getProperty(USER_PROPERTY_NAME) != null)
 		user = System.getProperty(USER_PROPERTY_NAME);
-	    
+
 	    if (System.getProperty(PASSWORD_PROPERTY_NAME) != null)
 		password = System.getProperty(PASSWORD_PROPERTY_NAME);
-	    
+
 	    //workaround for some buggy JVMs, that don't load the driver automatically (like GCJ)
 	    Class.forName("org.h2.Driver");
-	    
+
 	    return new H2DbBibleStorage(DriverManager.getConnection(url, user, password));
-	
+
 	} catch (SQLException e) {
 	    logger.error("Exception caught when creating H2DbBibleStorage", e);
 	    throw new BibleStorageException("BibleStorage could not be created", e);
